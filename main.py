8848b61172c36c3fb31e98eed26ce2feb1b788f6
Hang Yu <hyu@Elsas-MacBook-Air.local>
2023-06-07 22:52:10 -0700

    update

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
+    This is an update!
     ### Want to learn more?
     - Check out [streamlit.io](https://streamlit.io)
     - Jump into our [documentation](https://docs.streamlit.io)
